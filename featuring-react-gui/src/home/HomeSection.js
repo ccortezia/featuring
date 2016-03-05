b7d811f2bcebd768614e0f2ef749da0fc24fc1aa
Cristiano <cristiano.cortezia@gmail.com>
2016-03-05 18:39:40 -0300

    feat(gui/react/header): logout button and logged username
    - token is resolved into a session record from an api
      call made easly on when HomeSection is mounted.
    
    - logged username is retrieved from the resolved token
      and printed on the header.
    
    - logout button destroy token synchronously and navigate
      away to /login when clicked.

--- featuring-react-gui/src/home/HomeSection.js
+++ featuring-react-gui/src/home/HomeSection.js
@@ -2,16 +2,28 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Header} from 'app/header';
 import {Footer} from 'app/footer';
+import store from 'app/root/store';
+import {requestSessionDetailAsyncAction} from 'app/session/actions';
 
 
-export function HomeSection({children}) {
-  return (
-    <div className="base home">
-      <Header />
-      {children}
-      <Footer />
-    </div>
-  );
+export class HomeSection extends React.Component {
+  constructor({children}) {
+      super({children});
+  }
+
+  componentWillMount() {
+    store.dispatch(requestSessionDetailAsyncAction());
+  }
+
+  render() {
+    return (
+      <div className="base home">
+        <Header />
+        {this.props.children}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default connect()(HomeSection);
