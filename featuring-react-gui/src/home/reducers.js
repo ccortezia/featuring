ce6783881b5ffa5871f3609cbc3b57781ce5f101
Cristiano <cristiano.cortezia@gmail.com>
2016-03-07 09:18:49 -0300

    fix(gui/react/home): ensure menu collapsing
    - menu should be collapsed upon navigation
    - menu should be collapsed upon login success
    
    fixes #23

--- featuring-react-gui/src/home/reducers.js
+++ featuring-react-gui/src/home/reducers.js
@@ -1,9 +1,13 @@
+import * as CT from 'app/header/constants';
 
 export function homeReducer(state={menuOpened: false}, action) {
 
   switch(action.type) {
-    case 'TOGGLE_NAVBAR':
+    case CT.TOGGLE_NAVBAR:
       return {menuOpened: !state.menuOpened};
+
+    case '@@router/LOCATION_CHANGE':
+      return {menuOpened: false};
 
     default:
       return state;
